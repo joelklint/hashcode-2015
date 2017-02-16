8c5feea240120a650189cdb60fdcf71010b53621
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-02-16 19:35:19 +0100

    classes

---
+++ hashcode-2015/src/Server.java
@@ -0,0 +1,15 @@
+
+public class Server {
+	public int id;
+	public int size;
+	public int capacity;
+	public int pool;
+	
+	
+	public Server(int id, int size, int capacity, int pool){
+		id = 0;
+		size = 0;
+		capacity = 0;
+		pool = 0;
+	}
+}
