580cac7cf396cac1aafc8e0b929e1b603a373174
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-02-16 20:23:33 +0100

    reader done

--- hashcode-2015/src/Main.java
+++ hashcode-2015/src/Main.java
@@ -1,4 +1,11 @@
+import java.io.File;
 
 public class Main {
-
+	public Reader read;
+	
+	public static void main(String[] args) {
+		File file = new File("./dc.in");
+		Reader read = new Reader(file);
+		read.readFile();
+	}
 }
--- hashcode-2015/src/Reader.java
+++ hashcode-2015/src/Reader.java
@@ -1,4 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Reader {
+	public Scanner scan;
+	public int id;
+	public Server server;
+	public Server[] servers;
+	public int[][] unavailable;
+	public int size;
+	public int capacity;
+	public int row;
+	public int col;
+	
+	public Reader(File file){
+		unavailable = new int[16][100];
+		servers = new Server[625];
+		id = 0;
+		try {
+			scan = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void readFile(){
+		row = 0;
+		col = 0;
+		scan.nextLine();
+		for(int i = 0; i < 80; i++){
+			row = scan.nextInt();
+			col = scan.nextInt();
+			unavailable[row][col] = -1;
+		}
+		
+		while(scan.hasNextInt()){
+			size = scan.nextInt();
+			capacity = scan.nextInt();
+			Server newServer = new Server(id, size, capacity, 0);
+			servers[id] = newServer;
+			System.out.println(servers[id].capacity);
+			id++;
+	
+		}
+	}
+		
+	}
 
-}
